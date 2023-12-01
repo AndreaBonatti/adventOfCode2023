726ae5572eeb463edeff84e62737681b7e980dce
Andrea Bonatti <andreabonatti92@gmail.com>
2023-12-01 17:46:58 +0100

    A more elegant solution to the problem that does not work properly for the part 2 (give the solution of 53363 instead of 53340)

--- main.py
+++ main.py
@@ -1,39 +1,26 @@
 def get_calibration_value_of_a_line(line: str) -> int:
-    valid_numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-                     'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero'}
-    first_digit, last_digit = -1, -1
-    start, end = 0, 0
-    for position in range(len(line)):
-        end = position
-        if line[position].isdigit():
-            first_digit = line[position]
-            break
-        for i in range(start, end):
-            if line[i:end + 1] in valid_numbers:
-                first_digit = line[i:end + 1]
-                break
-        if first_digit in valid_numbers:
-            break
-    start, end = len(line) - 1, len(line) - 1
-    for position in reversed(range(len(line))):
-        start = position - 1
-        if line[position].isdigit():
-            last_digit = line[position]
-            break
-        for i in reversed(range(start + 1, end + 2)):
-            if line[start + 1:i] in valid_numbers:
-                last_digit = line[start + 1:i]
-                break
-        if last_digit in valid_numbers:
-            break
-    literal_to_number = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+    literal_to_number = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5,
+                         '6': 6, '7': 7, '8': 8, '9': 9, '0': 0,
+                         'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
                          'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'zero': 0}
-    if str(first_digit) in literal_to_number.keys():
-        first_digit = literal_to_number[str(first_digit)]
-    if str(last_digit) in literal_to_number.keys():
-        last_digit = literal_to_number[str(last_digit)]
-    result = int(first_digit) * 10 + int(last_digit)
-    return result
+    start = 0
+    numbers = []
+    for position in range(1, len(line) + 1):
+        # keys = everything we consider as a valid number
+        possible_number = line[start:position]
+        if possible_number in literal_to_number.keys():
+            numbers.append(literal_to_number[possible_number])
+            start = position
+        else:
+            next_position = start + 1
+            while next_position < position:
+                possible_number_2 = line[next_position:position]
+                if possible_number_2 in literal_to_number.keys():
+                    numbers.append(literal_to_number[possible_number_2])
+                    start = position
+                    break
+                next_position += 1
+    return numbers[0] * 10 + numbers[-1]
 
 
 def get_sum_of_all_calibration_values():

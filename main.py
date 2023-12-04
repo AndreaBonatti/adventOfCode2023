bf45222e0c8671efa3b81de436495ed5b480169e
Andrea Bonatti <andreabonatti92@gmail.com>
2023-12-04 14:41:53 +0100

    Solution to the part 2 of the day 4 problem

--- main.py
+++ main.py
@@ -46,5 +46,35 @@ def get_pile_of_scratchcards_points():
     return result
 
 
+def get_total_scratchcards():
+    rows = get_file_rows('input.txt')
+    cards_dictionary = {}
+    max_card_number = len(rows) + 1
+    for i in range(1, max_card_number):
+        cards_dictionary[i] = 1
+    for index, row in enumerate(rows):
+        card_number = index + 1
+        scratchcard_parts = row.split(':')
+        numbers = scratchcard_parts[1].split('|')
+        winning_numbers = get_numbers_from_string(numbers[0])
+        played_numbers = get_numbers_from_string(numbers[1])
+        counter = 0
+        for played_number in played_numbers:
+            if played_number in winning_numbers:
+                counter += 1
+        if counter > 0:
+            # We do the same operation for the number of the same card that we have
+            # e.g. we have 2 cards n°2 => we add 2 times the winning cards from the card°2
+            for time in range(0,cards_dictionary[card_number]):
+                for i in range(card_number + 1, card_number + 1 + counter):
+                    if i < max_card_number:
+                        cards_dictionary[i] += 1
+    result = 0
+    for value in cards_dictionary.values():
+        result += value
+    return result
+
+
 if __name__ == '__main__':
     print(f'The large pile of colorful cards is worth: {get_pile_of_scratchcards_points()} points')
+    print(f'Total of scratch cards: {get_total_scratchcards()}')

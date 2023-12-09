1e91c249dd94172d34df5a1483f3b13df82b5e6e
Andrea Bonatti <andreabonatti92@gmail.com>
2023-12-09 12:49:55 +0100

    Solution to the 2nd part of the day 09 problem

--- main.py
+++ main.py
@@ -15,5 +15,23 @@ def get_sum_of_histories_values() -> int:
     return result
 
 
+def get_history_value_part_2(history: list[int]) -> int:
+    if all(value == 0 for value in history):
+        return 0
+    next_sequence = [b - a for a, b in zip(history, history[1:])]
+    difference = get_history_value_part_2(next_sequence)
+    return history[0] - difference
+
+
+def get_sum_of_histories_values_part_2() -> int:
+    result = 0
+    rows = open('input.txt')
+    for row in rows:
+        history = list(map(int, row.split()))
+        result += get_history_value_part_2(history)
+    return result
+
+
 if __name__ == '__main__':
     print(f'Sum of histories values: {get_sum_of_histories_values()}')
+    print(f'Sum of histories values part 2: {get_sum_of_histories_values_part_2()}')

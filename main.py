b6abc2b8239f4b1aef8fe497e90c90234cafeb5f
Andrea Bonatti <andreabonatti92@gmail.com>
2023-12-11 10:27:39 +0100

    Solution to the second part of the day 11 problem

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from collections import deque
 
+EXPANSION_FACTOR = 1000000
+
 
 def get_file_matrix(path: str) -> list[str]:
     with open(path) as file:
@@ -15,7 +17,7 @@ def get_flipped_matrix(matrix: list[str]) -> list[str]:
     return flipped_matrix
 
 
-def get_expanded_galaxies_by_rows(galaxies: list[str]):
+def get_expanded_galaxies_by_rows(galaxies: list[str]) -> list[str]:
     rows_to_expand = []
     for i, row in enumerate(galaxies):
         to_expand = True
@@ -40,7 +42,7 @@ def get_shortest_path_between_two_galaxies(galaxy_0: tuple[int, int], galaxy_1: 
     return diff_x + diff_y
 
 
-def get_sum_shortest_paths_between_all_galaxies():
+def get_sum_shortest_paths_between_all_galaxies() -> int:
     galaxies = get_file_matrix('input.txt')
     galaxies_expanded_by_rows = get_expanded_galaxies_by_rows(galaxies)
     # Flip the matrix to re-use the same expansion function
@@ -65,5 +67,60 @@ def get_sum_shortest_paths_between_all_galaxies():
     return result
 
 
+def get_galaxies_rows_to_expand(galaxies: list[str]) -> list[int]:
+    rows_to_expand = []
+    for i, row in enumerate(galaxies):
+        to_expand = True
+        for char in row:
+            if char != '.':
+                to_expand = False
+                break
+        if to_expand:
+            rows_to_expand.append(i)
+    return rows_to_expand
+
+
+def get_shortest_path_between_two_galaxies_part_2(
+        galaxy_0: tuple[int, int], galaxy_1: tuple[int, int], rows_to_expand: list[int], columns_to_expand: list[int]
+) -> int:
+    total = 0
+    x1, y1 = galaxy_0
+    x2, y2 = galaxy_1
+    # Control of every point between the 2 galaxies:
+    # if it is a point to expand => add the EXPANSION_FACTOR
+    # else => add 1
+    for x in range(min(x1, x2), max(x1, x2)):
+        total += EXPANSION_FACTOR if x in rows_to_expand else 1
+    for y in range(min(y1, y2), max(y1, y2)):
+        total += EXPANSION_FACTOR if y in columns_to_expand else 1
+    return total
+
+
+def get_sum_shortest_paths_between_all_galaxies_part_2() -> int:
+    galaxies = get_file_matrix('input.txt')
+    rows_to_expand = get_galaxies_rows_to_expand(galaxies)
+    # Flip the matrix to re-use the same expansion function
+    flipped_galaxies = get_flipped_matrix(galaxies)
+    columns_to_expand = get_galaxies_rows_to_expand(flipped_galaxies)
+    # Find all the galaxies positions
+    galaxies_positions = {}
+    galaxy_index = 1
+    for i, row in enumerate(galaxies):
+        for j, char in enumerate(row):
+            if char != '.':
+                galaxies_positions[galaxy_index] = (i, j)
+                galaxy_index += 1
+    # Calculate the distance between every galaxy
+    result = 0
+    queue = deque(galaxies_positions.values())
+    while len(queue) > 1:
+        current = queue.pop()
+        for galaxy in queue:
+            result += get_shortest_path_between_two_galaxies_part_2(current, galaxy, rows_to_expand, columns_to_expand)
+    return result
+
+
 if __name__ == '__main__':
-    print(f'Sum of the shortest paths between all the galaxies: {get_sum_shortest_paths_between_all_galaxies()}')
+    # print(f'Sum of the shortest paths between all the galaxies: {get_sum_shortest_paths_between_all_galaxies()}')
+    print(f'Sum of the shortest paths between all the galaxies with expansion factor:'
+          f' {get_sum_shortest_paths_between_all_galaxies_part_2()}')

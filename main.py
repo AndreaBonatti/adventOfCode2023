62cdd1405ca7122e40aaae78958cc9c73d6fab89
Andrea Bonatti <andreabonatti92@gmail.com>
2023-12-13 09:44:39 +0100

    Solution to the 1st part of day 13 problem

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_flipped_matrix(matrix: list[str]) -> list[str]:
+    flipped_matrix = [''] * len(matrix[0])
+    for i, row in enumerate(matrix):
+        for j, char in enumerate(row):
+            flipped_matrix[j] += char
+    return flipped_matrix
 
 
-# Press the green button in the gutter to run the script.
+def get_separator(matrix: list[str]) -> int:
+    for separator in range(1, len(matrix)):
+        above = matrix[:separator][::-1]  # flipped to do the equality check
+        below = matrix[separator:]
+        # make the two part of the same length
+        above = above[:len(below)]
+        below = below[:len(above)]
+        if above == below:
+            return separator
+    return 0
+
+
+def get_number_from_patterns_notes() -> int:
+    result = 0
+    with open('input.txt') as file:
+        patterns = file.read().split('\n\n')
+        for pattern in patterns:
+            matrix = pattern.splitlines()
+            # row reflection
+            row = get_separator(matrix)
+            result += row * 100
+            # column reflection
+            column = get_separator(get_flipped_matrix(matrix))
+            result += column
+    return result
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(f'Part 1 number: {get_number_from_patterns_notes()}')

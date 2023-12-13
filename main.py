a264b59a7082c75e4d6e8c9f34fd6fbb81ac61f9
Andrea Bonatti <andreabonatti92@gmail.com>
2023-12-13 10:03:56 +0100

    Solution to the 2nd part of day 13 problem

--- main.py
+++ main.py
@@ -33,5 +33,31 @@ def get_number_from_patterns_notes() -> int:
     return result
 
 
+def get_separator_considering_smudges(matrix: list[str]) -> int:
+    for separator in range(1, len(matrix)):
+        above = matrix[:separator][::-1]  # flipped to do the equality check
+        below = matrix[separator:]
+        # if the number of difference between the two part are exactly one
+        if sum(sum(0 if i == j else 1 for i, j in zip(x, y)) for x, y in zip(above, below)) == 1:
+            return separator
+    return 0
+
+
+def get_number_from_patterns_notes_considering_smudges() -> int:
+    result = 0
+    with open('input.txt') as file:
+        patterns = file.read().split('\n\n')
+        for pattern in patterns:
+            matrix = pattern.splitlines()
+            # row reflection
+            row = get_separator_considering_smudges(matrix)
+            result += row * 100
+            # column reflection
+            column = get_separator_considering_smudges(get_flipped_matrix(matrix))
+            result += column
+    return result
+
+
 if __name__ == '__main__':
     print(f'Part 1 number: {get_number_from_patterns_notes()}')
+    print(f'Part 2 number: {get_number_from_patterns_notes_considering_smudges()}')

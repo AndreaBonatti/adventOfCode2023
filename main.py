b7dd04f158fd857dc65b1985c1468d347d764e6a
Andrea Bonatti <andreabonatti92@gmail.com>
2023-12-14 10:55:13 +0100

    Solution to the part 1 of the day 14 problem

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_file_matrix(path: str) -> list[str]:
+    with open(path) as file:
+        data = file.read()
+        return data.strip().splitlines()
 
 
-# Press the green button in the gutter to run the script.
+def get_flipped_matrix(matrix: list[str]) -> list[str]:
+    flipped_matrix = [''] * len(matrix[0])
+    for i, row in enumerate(matrix):
+        for j, char in enumerate(row):
+            flipped_matrix[j] += char
+    return flipped_matrix
+
+
+def get_total_load_on_the_north_support_beams() -> int:
+    file = get_file_matrix('input.txt')
+    flipped_file = get_flipped_matrix(file)
+    # Sorting rocks
+    sorted_flipped_file = []
+    for row in flipped_file:
+        groups = []
+        for group in row.split('#'):
+            groups.append("".join(list(sorted(group, reverse=True))))
+        sorted_flipped_file.append("#".join(groups))
+    sorted_file = get_flipped_matrix(sorted_flipped_file)
+    # print(sorted_file)
+    result = 0
+    for i, row in enumerate(sorted_file):
+        result += row.count('O') * (len(sorted_file) - i)
+    return result
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(f'Part 1: total load on the north support beams = {get_total_load_on_the_north_support_beams()}')

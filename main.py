a98a0688d8349eb379f4df7b41b7e7e2c2e0b09a
Andrea Bonatti <andreabonatti92@gmail.com>
2023-12-14 11:17:38 +0100

    Solution to the part 2 of the day 14 problem(https://www.youtube.com/watch?v=WCVOBKUNc38)

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+NUMBER_OF_CYCLES = 1000000000
+
+
 def get_file_matrix(path: str) -> list[str]:
     with open(path) as file:
         data = file.read()
@@ -30,5 +33,42 @@ def get_total_load_on_the_north_support_beams() -> int:
     return result
 
 
+def spin_cycle(file: list[str]) -> list[str]:
+    for _ in range(4):
+        flipped_file = tuple(get_flipped_matrix(file))
+        # Sorting rocks
+        sorted_flipped_file = []
+        for row in flipped_file:
+            groups = []
+            for group in row.split('#'):
+                groups.append("".join(list(sorted(tuple(group), reverse=True))))
+            sorted_flipped_file.append("#".join(groups))
+        sorted_file = tuple([row[::-1] for row in tuple(sorted_flipped_file)])
+        file = sorted_file
+
+    return file
+
+
+def get_total_load_on_the_north_support_beams_part_two() -> int:
+    file = tuple(get_file_matrix('input.txt'))
+    already_seen = {file}
+    array = [file]
+    counter = 0
+    while True:
+        counter += 1
+        file = spin_cycle(file)
+        if file in already_seen:
+            break
+        already_seen.add(file)
+        array.append(file)
+    first = array.index(file)
+    file = array[(NUMBER_OF_CYCLES - first) % (counter - first) + first]
+    result = 0
+    for i, row in enumerate(file):
+        result += row.count('O') * (len(file) - i)
+    return result
+
+
 if __name__ == '__main__':
-    print(f'Part 1: total load on the north support beams = {get_total_load_on_the_north_support_beams()}')
+    # print(f'Part 1: total load on the north support beams = {get_total_load_on_the_north_support_beams()}')
+    print(f'Part 2: total load on the north support beams = {get_total_load_on_the_north_support_beams_part_two()}')

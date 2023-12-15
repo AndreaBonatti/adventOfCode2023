d1c6c75852138105d440a14775d36f9b19cf9332
Andrea Bonatti <andreabonatti92@gmail.com>
2023-12-15 11:01:25 +0100

    Solution to the part 2 of day 15 problem

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def get_hash_value(word: str) -> int:
     return current_value
 
 
-def get_sum_of_all_hash_values():
+def get_sum_of_all_hash_values() -> int:
     result = 0
     rows = get_file_rows('input.txt')
     for row in rows:
@@ -26,5 +26,34 @@ def get_sum_of_all_hash_values():
     return result
 
 
+def get_focusing_power() -> int:
+    result = 0
+    rows = get_file_rows('input.txt')
+    boxes = [[] for _ in range(256)]
+    focal_lengths = {}
+    # Hashmap to elaborate all the file data
+    for row in rows:
+        if '-' in row:
+            label = row[:-1]
+            box_number = get_hash_value(label)
+            if label in boxes[box_number]:
+                boxes[box_number].remove(label)
+        else:
+            label, length = row.split("=")
+            length = int(length)
+
+            index = get_hash_value(label)
+            if label not in boxes[index]:
+                boxes[index].append(label)
+
+            focal_lengths[label] = length
+    # Sum of each the focusing power of each lens
+    for box_number, box in enumerate(boxes, 1):
+        for slot, label in enumerate(box, 1):
+            result += box_number * slot * focal_lengths[label]
+    return result
+
+
 if __name__ == '__main__':
     print(f'Part 1: sum = {get_sum_of_all_hash_values()}')
+    print(f'Part 2: focusing power = {get_focusing_power()}')

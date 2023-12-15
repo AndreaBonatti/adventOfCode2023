7d6dc04bb02249181c659e3444e71e34dd8b1d3d
Andrea Bonatti <andreabonatti92@gmail.com>
2023-12-15 10:33:43 +0100

    Solution to the part 1 of day 15 problem

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_file_rows(path: str) -> list[str]:
+    with open(path) as file:
+        data = file.read()
+        return data.strip().split(',')
 
 
-# Press the green button in the gutter to run the script.
+def get_hash_value(word: str) -> int:
+    current_value = 0
+    for char in word:
+        # Determine the ASCII code for the current character of the string.
+        ascii_code = ord(char)
+        # Increase the current value by the ASCII code you just determined.
+        current_value += ascii_code
+        # Set the current value to itself multiplied by 17.
+        current_value *= 17
+        # Set the current value to the remainder of dividing itself by 256.
+        current_value %= 256
+    return current_value
+
+
+def get_sum_of_all_hash_values():
+    result = 0
+    rows = get_file_rows('input.txt')
+    for row in rows:
+        result += get_hash_value(row)
+    return result
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(f'Part 1: sum = {get_sum_of_all_hash_values()}')

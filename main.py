9ad20266cc536ca471d8ed07cae6eee256a35abc
Andrea Bonatti <andreabonatti92@gmail.com>
2023-12-18 10:51:04 +0100

    Solution to the 1st part of the problem of day-18

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Up, down, left, right
+DIRECTIONS = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_meters_to_fill_the_lagoon() -> int:
+    boundary_points = 0
+    points = [(0, 0)]  # starting point = (0, 0)
+    with open('input.txt') as file:
+        for row in file:
+            direction, step_number, _ = row.split()  # we ignore the RGB info
+            row_direction, column_direction = DIRECTIONS[direction]
+            step_number = int(step_number)
+            boundary_points += step_number
+            # Add a new vertex of the polygon
+            row, column = points[-1]
+            points.append((row + row_direction * step_number, column + column_direction * step_number))
+    # https://en.wikipedia.org/wiki/Shoelace_formula
+    area = abs(
+        sum(points[i][0] * (points[i - 1][1] - points[(i + 1) % len(points)][1]) for i in range(len(points)))
+    ) // 2
+    # https://en.wikipedia.org/wiki/Pick%27s_theorem
+    internal_points = area - boundary_points // 2 + 1
+    return internal_points + boundary_points
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(f'Part 1: cubic meters of lava to fill the lagoon = {get_meters_to_fill_the_lagoon()}')

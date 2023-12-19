a5bd8fcd9508cd95858835efd52d949b48173425
Andrea Bonatti <andreabonatti92@gmail.com>
2023-12-19 11:04:47 +0100

    Solution to the 2nd part of the problem of day-19

--- main.py
+++ main.py
@@ -44,5 +44,55 @@ def get_sum_rating_numbers_accepted_parts() -> int:
     return result
 
 
+def get_distinct_combinations_accepted() -> int:
+    workflows, ratings = get_workflows_and_ratings('input.txt')
+    workflows_dictionary = {}
+    for row in workflows:
+        name, rest = row[:-1].split('{')
+        rules = rest.split(',')
+        workflows_dictionary[name] = ([], rules.pop())
+        for rule in rules:
+            comparison, target = rule.split(':')
+            key = comparison[0]
+            operator = comparison[1]
+            number = int(comparison[2:])
+            workflows_dictionary[name][0].append((key, operator, number, target))
+
+    def count(ranges, item_name="in"):
+        if item_name == "R":
+            return 0
+        if item_name == "A":
+            product = 1
+            for lower_number, higher_number in ranges.values():
+                product *= higher_number - lower_number + 1
+            return product
+        workflows_rules, fallback = workflows_dictionary[item_name]
+        total = 0
+        for k, op, n, t in workflows_rules:
+            lower_number, higher_number = ranges[k]
+            if op == "<":
+                accepted = (lower_number, n - 1)
+                not_accepted = (n, higher_number)
+            else:
+                accepted = (n + 1, higher_number)
+                not_accepted = (lower_number, n)
+            if accepted[0] <= accepted[1]:
+                copy = dict(ranges)
+                copy[k] = accepted
+                total += count(copy, t)
+            if not_accepted[0] <= not_accepted[1]:
+                ranges = dict(ranges)
+                ranges[k] = not_accepted
+            else:
+                break
+        else:
+            total += count(ranges, fallback)
+        return total
+
+    return count({key: (1, 4000) for key in "xmas"})
+
+
 if __name__ == '__main__':
     print(f'Part 1 - Sum of rating number of all accepted parts: {get_sum_rating_numbers_accepted_parts()}')
+    print(f'Part 2 - Number of distinct combinations of ratings accepted by the Elves\' workflows: '
+          f'{get_distinct_combinations_accepted()}')
